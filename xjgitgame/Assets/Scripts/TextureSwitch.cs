0cd0133dddb6723287c823890d03698f63039b5b
JosephWong333 <76153374+JosephWong333@users.noreply.github.com>
2022-06-29 22:42:11 -0700

    Replaced w/ Proto
    Made a project (tests) previously not backed up with version control. Replacing files from this one to the previous one.

---
+++ xjgitgame/Assets/Scripts/TextureSwitch.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureSwitch : MonoBehaviour
+{
+    [SerializeField]
+    float change_speed = 0.1f;
+
+    [SerializeField]
+    Material og_mat;
+
+    [SerializeField]
+    Material new_mat;
+
+    [SerializeField]
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(timer(10f));
+    }
+
+    IEnumerator timer(float time)
+    {
+        float i = 0;
+        float rate = 1 / time;
+
+        while (i < time)
+        {
+            rend.sharedMaterial = new_mat;
+            yield return new WaitForSeconds(change_speed);
+            rend.sharedMaterial = og_mat;
+            yield return new WaitForSeconds(change_speed);
+            i += rate * Time.deltaTime;
+            Debug.Log("iter");
+        }
+        StartCoroutine(timer(10f));
+
+    }
+}
