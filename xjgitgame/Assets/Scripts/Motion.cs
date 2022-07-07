ac49f2e1b71b37eedefe14cfc3b6d9b6575a1d8b
JosephWong333 <76153374+JosephWong333@users.noreply.github.com>
2022-07-07 12:58:34 -0700

    cambob
    checkpoint
    adding colliders

--- xjgitgame/Assets/Scripts/Motion.cs
+++ xjgitgame/Assets/Scripts/Motion.cs
@@ -7,6 +7,7 @@ public class Motion : MonoBehaviour
     public static float lookSpeed = 3;
     public float MoveSpeed = 3;
     public float SprintSpeed = 5.25f;
+    bool isSprinting = false;
     private Vector2 rotation = Vector2.zero;
     CharacterController cc;
 
@@ -19,6 +20,15 @@ public class Motion : MonoBehaviour
     public float jumpHeight = 1.25f;
     public bool canJump = true;
 
+    //headbob stuff
+    //- camera bob
+    public float walkBobSpeed = 14f;
+    public float walkBobAmount = 0.05f;
+    public float sprintBobSpeed = 18f;
+    public float sprintBobAmount = 0.1f;
+    float defaultYPos = 0;
+    float timer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,8 +39,9 @@ public class Motion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
         Look();
+        Move();
+        Cambob();
     }
 
     public void Move() // Look rotation (UP down is Camera) (Left right is Transform rotation)
@@ -63,6 +74,7 @@ public class Motion : MonoBehaviour
         if (isGrounded && Input.GetButton("Jump") && canJump) {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
+
     }
 
 
@@ -74,4 +86,21 @@ public class Motion : MonoBehaviour
         transform.eulerAngles = new Vector2(0, rotation.y) * lookSpeed;
         Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * lookSpeed, 0, 0);
     }
+
+    void Cambob()
+    {
+        if (!isGrounded) return; //if player jumped then no headbob
+
+        if (Mathf.Abs(cc.velocity.magnitude) > 0)
+        {
+            timer += Time.deltaTime * (isSprinting ? sprintBobSpeed : !isSprinting ? walkBobSpeed : walkBobSpeed); //good way of cutting down on nested if statements
+            Camera.main.transform.localPosition = new Vector3(
+                Camera.main.transform.localPosition.x,
+                defaultYPos += Time.deltaTime * Mathf.Sin(timer) * (isSprinting ? sprintBobAmount : !isSprinting ? walkBobAmount : walkBobAmount),
+                Camera.main.transform.localPosition.z
+                );
+        }
+
+    }
+
 }
