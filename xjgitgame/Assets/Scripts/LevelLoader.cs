e0cbeeb6cc512eadfb7d2fc0212bc49d07663a25
JosephWong333 <76153374+JosephWong333@users.noreply.github.com>
2022-07-07 23:17:12 -0700

    Menu UI Colliders
    lel

---
+++ xjgitgame/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField]
+    int to_scene_index = 0;
+
+    [SerializeField]
+    bool updater = false;
+
+    public Animator transition;
+    public float transitionTime = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && updater)
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(timer());
+    }
+
+    IEnumerator timer()
+    {
+        // Play anim
+        transition.SetTrigger("start");
+        // Wait
+        yield return new WaitForSeconds(transitionTime);
+        // Load scene
+        SceneManager.LoadScene(to_scene_index);
+
+    }
+}
