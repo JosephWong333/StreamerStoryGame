8b1034ae0b7a46ec46f75852a1f45108a82ffef7
JosephWong333 <76153374+JosephWong333@users.noreply.github.com>
2022-07-17 07:30:33 +0400

    gribbon + npc ai func
    add grilling

---
+++ xjgitgame/Assets/Scripts/npcanims.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class npcanims : MonoBehaviour
+{
+    [SerializeField]
+    NavMeshAgent nma;
+
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    Animator anim;
+
+    [SerializeField]
+    string trigger2;
+
+    bool canMove = true;
+
+    [SerializeField]
+    float closeamount = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nma = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            print("moving");
+            nma.SetDestination(target.position);
+            if (Vector3.Distance(transform.position, target.position) < closeamount)
+            {
+                print("touched player");
+                canMove = false;
+                anim.SetTrigger(trigger2);
+            }
+
+        }
+           
+    }
+
+}
