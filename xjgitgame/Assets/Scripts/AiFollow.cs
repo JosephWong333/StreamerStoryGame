51da62fdeab100b1d1c39751253b9d8e10085330
JosephWong333 <76153374+JosephWong333@users.noreply.github.com>
2022-07-18 11:25:36 +0400

    More obstacles + copcar ai

---
+++ xjgitgame/Assets/Scripts/AiFollow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiFollow : MonoBehaviour
+{
+    [SerializeField]
+    NavMeshAgent nma;
+
+    [SerializeField]
+    Transform target;
+
+    public bool canGo = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nma = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canGo)
+            nma.SetDestination(target.position);
+    }
+}
