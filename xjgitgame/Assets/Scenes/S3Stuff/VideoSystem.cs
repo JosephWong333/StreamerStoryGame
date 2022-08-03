c65ae5b617e94c78cacb6c44e22d21be41b5f714
JosephWong333 <76153374+JosephWong333@users.noreply.github.com>
2022-08-04 01:27:40 +0800

    added chkpt
    - working Running Mamo using preexisting AI script (may need 2 adjust vals)
    - Made Video Examples
    - Made Audio Switcher in S1

---
+++ xjgitgame/Assets/Scenes/S3Stuff/VideoSystem.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.Audio;
+
+public class VideoSystem : MonoBehaviour
+{
+
+    [SerializeField]
+    bool Prep_WhenDone_Shoot = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<VideoPlayer>().Prepare();
+
+        if (Prep_WhenDone_Shoot)
+            StartCoroutine(EShoot());
+    }
+
+    IEnumerator EShoot()
+    {
+
+        GetComponent<RawImage>().enabled = true;
+
+        VideoPlayer vp = GetComponent<VideoPlayer>();
+
+        while (!vp.isPrepared)
+        {
+            yield return null;
+        }
+
+        vp.Play();
+        //external audio? maybe later
+        //GetComponent<AudioSource>().Play();
+
+    }
+
+    public void Shoot()
+    {
+        StartCoroutine(EShoot());
+    }
+
+    //Shoot w new scene may be done later
+}
---
+++ xjgitgame/Assets/Scripts/ChangeAudio.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAudio : MonoBehaviour
+{
+    // Trigger-based
+    [Header("Audio Data")]
+    [SerializeField] private GameObject Audio1;
+    [SerializeField] private GameObject Audio2;
+    [Header("IF Data")]
+    [SerializeField] private bool destroySelf = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (Audio1.activeInHierarchy == true)
+            {
+                Audio1.SetActive(false);
+                Audio2.SetActive(true);
+                if (destroySelf) { Destroy(gameObject); }
+            }
+            else
+            {
+                Audio1.SetActive(true);
+                Audio2.SetActive(false);
+                if (destroySelf) { Destroy(gameObject); }
+            }
+        }
+    }
+
+}
