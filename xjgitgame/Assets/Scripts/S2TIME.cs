827b68102b19de045c2e047092d2a978ab1029f2
JosephWong333 <76153374+JosephWong333@users.noreply.github.com>
2022-08-10 16:11:25 +0800

    prototype TIMER s2

---
+++ xjgitgame/Assets/Scripts/S2TIME.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class S2TIME : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text TimerText;
+
+    public int timeLeft = 100;
+    public bool working = true;
+
+    // Start is called before the first frame update
+    IEnumerator Timing()
+    {
+        while (timeLeft > 0 && working == true)
+        {
+            yield return new WaitForSeconds(1f);
+            if (working == true)
+                timeLeft -= 1;
+        }
+        // When done
+        End();
+    }
+
+    public void StopTimer()
+    {
+        working = false;
+    }
+
+    public void StartTimerBack()
+    {
+        working = true;
+        StartCoroutine(Timing());
+    }
+
+    // Update is called once per frame
+    public void End()
+    {
+        //safeguard
+        if (working == false) { return; }
+
+        working = false;
+        Debug.Log("DONENENEN111");
+    }
+
+    public void Update()
+    {
+        TimerText.text = "TIME LEFT: " + timeLeft;
+    }
+
+}
