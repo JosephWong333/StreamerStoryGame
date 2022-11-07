3c3030ad7b3749e074fdb5d30c0829ce57537ebe
JosephWong333 <76153374+JosephWong333@users.noreply.github.com>
2022-11-07 12:45:30 +0800

    donee
    just needs exporting

---
+++ xjgitgame/Assets/Scripts/Sens.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Sens : MonoBehaviour
+{
+
+    [SerializeField]
+    private TMP_Text SensTxt;
+
+    public void Add()
+    {
+        Motion.lookSpeed += 1;
+        MotionCar.lookSpeed += 1;
+    }
+
+    public void Minus()
+    {
+        if (Motion.lookSpeed > 0)
+        {
+            Motion.lookSpeed -= 1;
+            MotionCar.lookSpeed -= 1;
+        }
+
+    }
+
+    private void Update()
+    {
+        SensTxt.text = Motion.lookSpeed.ToString();
+    }
+}
